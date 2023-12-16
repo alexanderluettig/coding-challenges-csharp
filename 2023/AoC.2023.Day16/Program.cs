ded9a4342f5a96cb8811b1ac4df6d5c7cddaa79e
Alexander Lüttig <alexanderluettig@gmail.com>
2023-12-16 15:48:45 +0100

    i need to build my own point implementation

--- 2023/AoC.2023.Day16/Program.cs
+++ 2023/AoC.2023.Day16/Program.cs
@@ -4,140 +4,157 @@ namespace AoC._2023.Day16;
 
 internal class Program
 {
-    private const int FPS = 10;
-    private const int FrameTime = 1000 / FPS;
     private static async Task Main(string[] args)
     {
         await using var stream = typeof(Program).Assembly
         .GetManifestResourceStream(typeof(Program), "test.txt");
         using var reader = new StreamReader(stream!, Encoding.UTF8, leaveOpen: true);
 
-        string[][] map = [];
-        for (var line = await reader.ReadLineAsync(); line != null; line = await reader.ReadLineAsync())
+        var S = reader.ReadToEnd().Split(Environment.NewLine).ToList();
+
+        long answer1 = 0;
+        long answer2 = 0;
+
+        var currentbeams = new Queue<(int tox, int toy, int fromd)>();
+        currentbeams.Enqueue((0, 0, 0));
+        var map = new Dictionary<(char c, int d), int>();
+        map[('.', 0)] = 0;
+        map[('.', 1)] = 1;
+        map[('.', 2)] = 2;
+        map[('.', 3)] = 3;
+        map[('/', 0)] = 3;
+        map[('/', 1)] = 2;
+        map[('/', 2)] = 1;
+        map[('/', 3)] = 0;
+        map[('\\', 0)] = 1;
+        map[('\\', 1)] = 0;
+        map[('\\', 2)] = 3;
+        map[('\\', 3)] = 2;
+        map[('-', 0)] = 0;
+        map[('-', 1)] = 5;
+        map[('-', 2)] = 2;
+        map[('-', 3)] = 5;
+        map[('|', 0)] = 6;
+        map[('|', 1)] = 1;
+        map[('|', 2)] = 6;
+        map[('|', 3)] = 3;
+
+
+        answer1 = GetEnergyV2(S, currentbeams, map);
+
+        for (int i = 0; i < S.Count; i++)
         {
-            map = [.. map, line.ToCharArray().Select(c => c.ToString()).ToArray()];
+            currentbeams.Enqueue((0, i, 0));
+            var a1 = GetEnergyV2(S, currentbeams, map);
+            if (a1 > answer2) answer2 = a1;
+
+            currentbeams.Enqueue((S.Count - 1, i, 2));
+            a1 = GetEnergyV2(S, currentbeams, map);
+            if (a1 > answer2) answer2 = a1;
         }
 
-        int[][] lights = new int[map.Length][];
-        for (var i = 0; i < map.Length; i++)
+        for (int i = 0; i < S[0].Length; i++)
         {
-            lights[i] = new int[map[i].Length];
+            currentbeams.Enqueue((i, 0, 1));
+            var a1 = GetEnergyV2(S, currentbeams, map);
+            if (a1 > answer2) answer2 = a1;
+
+            currentbeams.Enqueue((i, S[0].Length - 1, 3));
+            a1 = GetEnergyV2(S, currentbeams, map);
+            if (a1 > answer2) answer2 = a1;
         }
 
-        TraverseMap(map, lights, (0, 0), (1, 0));
-
-        Console.WriteLine(lights.ToList().Select(l => l.Sum()).Sum());
+        Console.WriteLine($"Answer 1: {answer1}");
+        Console.WriteLine($"Answer 2: {answer2}");
     }
 
-    private static void TraverseMap(string[][] map, int[][] lights, (int x, int y) position, (int x, int y) direction)
+    static long GetEnergy(List<string> S, Queue<(int tox, int toy, int fromd)> currentbeams)
     {
+        var visited = new HashSet<(int x, int y, int fromdirection)>();
 
-        ((int x, int y), (int x, int y))[] todo = [(position, direction)];
-
-        while (todo.Length > 0)
+        while (currentbeams.Count > 0)
         {
-            var (pos, dir) = todo[0];
-            todo = todo[1..];
-
-            if (pos.x < 0 || pos.x >= map.Length || pos.y < 0 || pos.y >= map[0].Length)
+            var beam = currentbeams.Dequeue();
+            if (!visited.Contains(beam))
             {
-                continue;
+                visited.Add(beam);
+                switch (S[beam.toy][beam.tox])
+                {
+                    case '.':
+                        if (beam.fromd == 0 && beam.tox < S[0].Length - 1) currentbeams.Enqueue((beam.tox + 1, beam.toy, beam.fromd));
+                        else if (beam.fromd == 2 && beam.tox > 0) currentbeams.Enqueue((beam.tox - 1, beam.toy, beam.fromd));
+                        else if (beam.fromd == 1 && beam.toy < S.Count - 1) currentbeams.Enqueue((beam.tox, beam.toy + 1, beam.fromd));
+                        else if (beam.fromd == 3 && beam.toy > 0) currentbeams.Enqueue((beam.tox, beam.toy - 1, beam.fromd));
+                        break;
+                    case '/':
+                        if (beam.fromd == 3 && beam.tox < S[0].Length - 1) currentbeams.Enqueue((beam.tox + 1, beam.toy, 0));
+                        else if (beam.fromd == 1 && beam.tox > 0) currentbeams.Enqueue((beam.tox - 1, beam.toy, 2));
+                        else if (beam.fromd == 2 && beam.toy < S.Count - 1) currentbeams.Enqueue((beam.tox, beam.toy + 1, 1));
+                        else if (beam.fromd == 0 && beam.toy > 0) currentbeams.Enqueue((beam.tox, beam.toy - 1, 3));
+                        break;
+                    case '\\':
+                        if (beam.fromd == 1 && beam.tox < S[0].Length - 1) currentbeams.Enqueue((beam.tox + 1, beam.toy, 0));
+                        else if (beam.fromd == 3 && beam.tox > 0) currentbeams.Enqueue((beam.tox - 1, beam.toy, 2));
+                        else if (beam.fromd == 0 && beam.toy < S.Count - 1) currentbeams.Enqueue((beam.tox, beam.toy + 1, 1));
+                        else if (beam.fromd == 2 && beam.toy > 0) currentbeams.Enqueue((beam.tox, beam.toy - 1, 3));
+                        break;
+                    case '-':
+                        if (beam.fromd == 0 && beam.tox < S[0].Length - 1) currentbeams.Enqueue((beam.tox + 1, beam.toy, 0));
+                        else if (beam.fromd == 2 && beam.tox > 0) currentbeams.Enqueue((beam.tox - 1, beam.toy, 2));
+                        else if (beam.fromd == 1 || beam.fromd == 3)
+                        {
+                            if (beam.tox < S[0].Length - 1) currentbeams.Enqueue((beam.tox + 1, beam.toy, 0));
+                            if (beam.tox > 0) currentbeams.Enqueue((beam.tox - 1, beam.toy, 2));
+                        }
+                        break;
+                    case '|':
+                        if (beam.fromd == 1 && beam.toy < S.Count - 1) currentbeams.Enqueue((beam.tox, beam.toy + 1, 1));
+                        else if (beam.fromd == 3 && beam.toy > 0) currentbeams.Enqueue((beam.tox, beam.toy - 1, 3));
+                        else if (beam.fromd == 0 || beam.fromd == 2)
+                        {
+                            if (beam.toy < S.Count - 1) currentbeams.Enqueue((beam.tox, beam.toy + 1, 1));
+                            if (beam.toy > 0) currentbeams.Enqueue((beam.tox, beam.toy - 1, 3));
+
+                        }
+                        break;
+                }
             }
-
-            lights[pos.x][pos.y] = 1;
-            todo = [.. todo, .. GetNext(map, lights, pos, dir)];
-
-            Console.Clear();
-            lights.ToList().ForEach(l => Console.WriteLine(string.Join("", l.Select(i => i == 1 ? "#" : "."))));
-            Thread.Sleep(FrameTime);
-
         }
+
+        return visited.Select(v => (v.x, v.y)).ToHashSet().Count;
     }
-
-    private static ((int nextX, int nextY), (int dx, int dy))[] GetNext(string[][] map, int[][] lights, (int x, int y) position, (int x, int y) direction)
+    static long GetEnergyV2(List<string> S, Queue<(int tox, int toy, int fromd)> currentbeams, Dictionary<(char c, int d), int> map)
     {
-        if (map[position.y][position.x] == "." || map[position.y][position.x] == "-" && direction.x != 0 || map[position.y][position.x] == "|" && direction.y != 0)
+        var visited = new HashSet<(int x, int y, int fromdirection)>();
+        var move = new (int dx, int dy)[4] { (1, 0), (0, 1), (-1, 0), (0, -1) };
+
+        while (currentbeams.Count > 0)
         {
-            var nextPos = (position.x + direction.x, position.y + direction.y);
-            return [(nextPos, direction)];
-        }
-        else if (map[position.y][position.x] == "/" && direction.x == 1)
-        {
-            var nextPos = (position.x, position.y - 1);
-            var nextDir = (0, -1);
-            return [(nextPos, nextDir)];
-        }
-        else if (map[position.y][position.x] == "/" && direction.x == -1)
-        {
-            var nextPos = (position.x, position.y + 1);
-            var nextDir = (0, 1);
-            return [(nextPos, nextDir)];
-        }
-        else if (map[position.y][position.x] == "/" && direction.y == 1)
-        {
-            var nextPos = (position.x - 1, position.y);
-            var nextDir = (-1, 0);
-            return [(nextPos, nextDir)];
-        }
-        else if (map[position.y][position.x] == "/" && direction.y == -1)
-        {
-            var nextPos = (position.x + 1, position.y);
-            var nextDir = (1, 0);
-            return [(nextPos, nextDir)];
-        }
-        else if (map[position.y][position.x] == "\\" && direction.x == 1)
-        {
-            var nextPos = (position.x, position.y + 1);
-            var nextDir = (0, 1);
-            return [(nextPos, nextDir)];
-        }
-        else if (map[position.y][position.x] == "\\" && direction.x == -1)
-        {
-            var nextPos = (position.x, position.y - 1);
-            var nextDir = (0, -1);
-            return [(nextPos, nextDir)];
-        }
-        else if (map[position.y][position.x] == "\\" && direction.y == 1)
-        {
-            var nextPos = (position.x + 1, position.y);
-            var nextDir = (1, 0);
-            return [(nextPos, nextDir)];
-        }
-        else if (map[position.y][position.x] == "\\" && direction.y == -1)
-        {
-            var nextPos = (position.x - 1, position.y);
-            var nextDir = (-1, 0);
-            return [(nextPos, nextDir)];
-        }
-        else if (map[position.y][position.x] == "|" && direction.x != 0)
-        {
-            if (lights[position.y][position.x + 1] == 1 && lights[position.y][position.x - 1] == 1)
+            var beam = currentbeams.Dequeue();
+            if (!visited.Contains(beam))
             {
-                return [];
-            }
+                visited.Add(beam);
+                var newd = map[(S[beam.toy][beam.tox], beam.fromd)];
 
-            var nextPos1 = (position.x, position.y - 1);
-            var nextDir1 = (0, -1);
-            var nextPos2 = (position.x, position.y + 1);
-            var nextDir2 = (0, 1);
-            return [(nextPos1, nextDir1), (nextPos2, nextDir2)];
+                if (newd < 5) Addwork(currentbeams, move, beam, newd, S[0].Length, S.Count);
+                else
+                {
+                    var nd1 = newd == 5 ? 0 : 1;
+                    var nd2 = newd == 5 ? 2 : 3;
+                    Addwork(currentbeams, move, beam, nd1, S[0].Length, S.Count);
+                    Addwork(currentbeams, move, beam, nd2, S[0].Length, S.Count);
+                }
+            }
         }
-        else if (map[position.y][position.x] == "-" && direction.y != 0)
+
+        return visited.Select(v => (v.x, v.y)).ToHashSet().Count;
+
+        static void Addwork(Queue<(int tox, int toy, int fromd)> currentbeams, (int dx, int dy)[] move, (int tox, int toy, int fromd) beam, int newd, int maxx, int maxy)
         {
-            if (lights[position.y + 1][position.x] == 1 && lights[position.y - 1][position.x] == 1)
-            {
-                return [];
-            }
-
-            TraverseMap(map, lights, (position.x + 1, position.y), (1, 0));
-            TraverseMap(map, lights, (position.x - 1, position.y), (-1, 0));
-            var nextPos1 = (position.x + 1, position.y);
-            var nextDir1 = (1, 0);
-            var nextPos2 = (position.x - 1, position.y);
-            var nextDir2 = (-1, 0);
-            return [(nextPos1, nextDir1), (nextPos2, nextDir2)];
+            int newx = beam.tox + move[newd].dx;
+            int newy = beam.toy + move[newd].dy;
+            if (0 <= newx && newx < maxx && newy >= 0 && newy < maxy)
+                currentbeams.Enqueue((newx, newy, newd));
         }
-
-        return [];
     }
 }
